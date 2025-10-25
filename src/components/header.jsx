d386c50883f4a478f804b2aac9d6bb0733aef43d
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-10-25 23:19:32 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,21 @@
 import FloatingImages from "./FloatingImages";
 import { Link } from "react-router-dom";
+function Header({ hImage }) {
+  return (
+    <div
+      className="header"
+    >
+      <FloatingImages />
 
-function Header({hImage}){
-	return(
-	    <div className="header" >
-			<FloatingImages />
-            <div className="detail">
-	        <text id="name">💎ÑD COLLCTÏON</text>
-	        <text id="defination">💎 ND Collection Quality Never Questioned!</text>
-	        <button id="shopNow">Shop Now</button>
-	        </div>
-	    </div>
-	);
+      <div className="detail">
+        <text id="name">💎ÑD COLLCTÏON</text>
+        <text id="defination">💎 ND Collection Quality Never Questioned!</text>
+        <Link to="/product/All">
+          <button id="shopNow">Shop Now</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
+
 export default Header;
